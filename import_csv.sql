d005bab24787d04092988c8094ab8771a7efaf5c
dnlsfq <dan90@live.com.my>
2020-01-09 17:02:20 +0800

    'updated'

---
+++ import_csv.sql
@@ -0,0 +1,35 @@
+-- import csv to sqlite 
+--  set mode to csv 
+.mode csv
+
+DROP TABLE IF EXISTS recent_grads;
+-- CREATE TABLE recent_grads(
+--     -- "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
+--     "RANK" INTEGER,
+--     "MAJOR_CODE" INTEGER,
+--     "MAJOR" TEXT,
+--     "TOTAL" INTEGER,
+--     "MEN" INTEGER,
+--     "WOMEN" INTEGER,
+--     "MAJOR_CATEGORY" TEXT,
+--     "SHAREWOMEN" REAL,
+--     "SAMPLE_SIZE" INTEGER,
+--     "EMPLOYED" INTEGER,
+--     "FULL_TIME" INTEGER,
+--     "PART_TIME" INTEGER,
+--     "FULL_TIME_YEAR_ROUND" INTEGER,
+--     "UNEMPLOYED" INTEGER,
+--     "UNEMPLOYMENT_RATE" INTEGER ,
+--     "MEDIAN" REAL,
+--     "P25TH" INTEGER,
+--     "P75TH" INTEGER,
+--     "COLLEGE_JOBS" INTEGER,
+--     "NON_COLLEGE_JOBS" INTEGER,
+--     "LOW_WAGE_JOBS" INTEGER
+-- );    
+
+.import C:/Users/Dan/Code/PYTHON/survey/recent-grads.csv recent_grads
+.mode column
+-- .headers on
+
+select * from recent_grads limit 1;
