6f9e789855e80a77361e2e8b9c5ddf55666bc6fc
dnlsfq <dan90@live.com.my>
2020-01-16 17:48:06 +0800

    'updated'

--- work.py
+++ work.py
@@ -1,2 +1,16 @@
 import sqlite3
-conn = sqlite3.connect(jobs.db)
+import pandas as pd 
+conn = sqlite3.connect('jobs.db')
+
+# Cursor instance 
+# cursor = conn.cursor()
+# query = "select Major from recent_grads;"
+# cursor.execute(query)
+# results = cursor.fetchall()
+
+# W/O Cursor instance
+query = "select Major from recent_grads;"
+results = conn.execute(query).fetchall()
+
+
+print(pd.DataFrame(results[0:2]))
